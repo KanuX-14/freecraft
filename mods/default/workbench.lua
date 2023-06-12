b4c4370df4f93fe649a010c3b31b166ac9b5a3ac
KanuX-14 <kanux.dev@gmail.com>
2023-06-12 17:18:41 -0300

    Automated workbench registration

--- mods/default/workbench.lua
+++ mods/default/workbench.lua
@@ -1,180 +1,74 @@
 -- default/workbench.lua
 
-local function can_dig(pos, player)
+local function check_inventory(pos, player)
 	local meta = engine.get_meta(pos);
 	local inv = meta:get_inventory()
 	return inv:is_empty("main")
 end
 
--- Acacia Workbench
-engine.register_node("default:acacia_workbench", {
-	description = "Workbench",
-	tiles = {
-		"default_acacia_workbench_top.png",
-		"default_acacia_wood.png",
-		"default_acacia_workbench_side.png"
-	},
-	groups = {workbench = 1, choppy = 2, oddly_breakable_by_hand = 2},
-	sounds = default.node_sound_wood_defaults(),
-	paramtype2 = "facedir",
-	can_dig = can_dig,
-	on_construct = function(pos)
-		local meta = engine.get_meta(pos)
-		meta:set_string("infotext", "Workbench")
-		meta:set_string("formspec", "size[9,9]"..
-						"list[current_player;craft;1.75,0.5;3,3;]"..
-						"list[current_player;craftpreview;5.75,1.5;1,1;]"..
-						"list[current_player;main;0,8;9,1;]"..
-						"list[current_player;main;0,5;9,3;9]"..
-						"image[4.75,1.5;1,1;sfinv_crafting_arrow.png]"..
-						"listring[current_player;main]"..
-						"listring[current_player;craft]")
-	end,
-})
-
-engine.register_craft({
-	output = "default:acacia_workbench",
-	recipe = {
-		{"default:acacia_wood", "default:acacia_wood"},
-		{"default:acacia_wood", "default:acacia_wood"},
+local function get_workbench()
+	local _formspec = {
+		type 		= "formspec",
+		info		=	"infotext",
+		layout 	= "size[9,9]" ..
+							"list[current_player;craft;1.75,0.5;3,3;]" ..
+							"list[current_player;craftpreview;5.75,1.5;1,1;]" ..
+							"list[current_player;main;0,8;9,1;]" ..
+							"list[current_player;main;0,5;9,3;9]" ..
+							"image[4.75,1.5;1,1;sfinv_crafting_arrow.png]" ..
+							"listring[current_player;main]" ..
+							"listring[current_player;craft]"
 	}
-})
-
--- Aspen Workbench
-engine.register_node("default:aspen_workbench", {
-	description = "Workbench",
-	tiles = {
-		"default_aspen_workbench_top.png",
-		"default_aspen_wood.png",
-		"default_aspen_workbench_side.png"
-	},
-	groups = {workbench = 1, choppy = 2, oddly_breakable_by_hand = 2},
-	sounds = default.node_sound_wood_defaults(),
-	paramtype2 = "facedir",
-	can_dig = can_dig,
-	on_construct = function(pos)
-		local meta = engine.get_meta(pos)
-		meta:set_string("infotext", "Workbench")
-		meta:set_string("formspec", "size[9,9]"..
-						"list[current_player;craft;1.75,0.5;3,3;]"..
-						"list[current_player;craftpreview;5.75,1.5;1,1;]"..
-						"list[current_player;main;0,8;9,1;]"..
-						"list[current_player;main;0,5;9,3;9]"..
-						"image[4.75,1.5;1,1;sfinv_crafting_arrow.png]"..
-						"listring[current_player;main]"..
-						"listring[current_player;craft]")
-	end,
-
-})
-
-engine.register_craft({
-	output = "default:aspen_workbench",
-	recipe = {
-		{"default:aspen_wood", "default:aspen_wood"},
-		{"default:aspen_wood", "default:aspen_wood"},
+	
+	local _workbench = {
+		name = "default:workbench",
+		description = "Workbench",
+		tiles = {
+			"default_workbench_top.png",
+			"default_wood.png",
+			"default_workbench_side.png"
+		},
+		groups = {
+			workbench = 1, choppy = 1,
+			oddly_breakable_by_hand = 2
+		},
+		sounds = default.node_sound_wood_defaults(),
+		paramtype2 = "facedir",
+		can_dig = check_inventory,
+		on_construct = function(pos)
+			local meta = engine.get_meta(pos)
+			meta:set_string(_formspec.info, description)
+			meta:set_string(_formspec.type, _formspec.layout)
+		end,
 	}
-})
 
--- Jungle Workbench
-engine.register_node("default:jungle_workbench", {
-	description = "Workbench",
-	tiles = {
-		"default_jungle_workbench_top.png",
-		"default_junglewood.png",
-		"default_jungle_workbench_side.png"
-	},
-	groups = {workbench = 1, choppy = 2, oddly_breakable_by_hand = 2},
-	sounds = default.node_sound_wood_defaults(),
-	paramtype2 = "facedir",
-	can_dig = can_dig,
-	on_construct = function(pos)
-		local meta = engine.get_meta(pos)
-		meta:set_string("infotext", "Workbench")
-		meta:set_string("formspec", "size[9,9]"..
-						"list[current_player;craft;1.75,0.5;3,3;]"..
-						"list[current_player;craftpreview;5.75,1.5;1,1;]"..
-						"list[current_player;main;0,8;9,1;]"..
-						"list[current_player;main;0,5;9,3;9]"..
-						"image[4.75,1.5;1,1;sfinv_crafting_arrow.png]"..
-						"listring[current_player;main]"..
-						"listring[current_player;craft]")
-	end,
+	return _workbench
+end
 
-})
+local ingredients = {
+	Acacia 	= "default:acacia_wood",	Apple 		= "default:wood",
+	Aspen 	= "default:aspen_wood",		Jungle 		= "default:junglewood",
+	Pine 		= "default:pine_wood"
+}
 
-engine.register_craft({
-	output = "default:jungle_workbench",
-	recipe = {
-		{"default:junglewood", "default:junglewood"},
-		{"default:junglewood", "default:junglewood"},
-	}
-})
+for wood, name in pairs(ingredients) do
+	local l_wood = string.lower(wood)
+	local _workbench = get_workbench()
 
--- Pine Workbench
-engine.register_node("default:pine_workbench", {
-	description = "Workbench",
-	tiles = {
-		"default_pine_workbench_top.png",
-		"default_pine_wood.png",
-		"default_pine_workbench_side.png"
-	},
-	groups = {workbench = 1, choppy = 2, oddly_breakable_by_hand = 2},
-	sounds = default.node_sound_wood_defaults(),
-	paramtype2 = "facedir",
-	can_dig = can_dig,
-	on_construct = function(pos)
-		local meta = engine.get_meta(pos)
-		meta:set_string("infotext", "Workbench")
-		meta:set_string("formspec", "size[9,9]"..
-						"list[current_player;craft;1.75,0.5;3,3;]"..
-						"list[current_player;craftpreview;5.75,1.5;1,1;]"..
-						"list[current_player;main;0,8;9,1;]"..
-						"list[current_player;main;0,5;9,3;9]"..
-						"image[4.75,1.5;1,1;sfinv_crafting_arrow.png]"..
-						"listring[current_player;main]"..
-						"listring[current_player;craft]")
-	end,
+	if (l_wood ~= "apple") then
+		_workbench.name = "default:" .. l_wood .. "_workbench"
+		_workbench.description = wood .. " Workbench"
+		_workbench.tiles = {
+			"default_" .. l_wood .. "_workbench_top.png",
+			"default_" .. l_wood .. "_wood.png",
+			"default_" .. l_wood .. "_workbench_side.png"
+		}
+	end
+	local X = name
 
-})
-
-engine.register_craft({
-	output = "default:pine_workbench",
-	recipe = {
-		{"default:pine_wood", "default:pine_wood"},
-		{"default:pine_wood", "default:pine_wood"},
-	}
-})
-
--- Apple Tree Workbench
-engine.register_node("default:workbench", {
-	description = "Workbench",
-	tiles = {
-		"default_workbench_top.png",
-		"default_wood.png",
-		"default_workbench_side.png"
-	},
-	groups = {workbench = 1, choppy = 2, oddly_breakable_by_hand = 2},
-	sounds = default.node_sound_wood_defaults(),
-	paramtype2 = "facedir",
-	can_dig = can_dig,
-	on_construct = function(pos)
-		local meta = engine.get_meta(pos)
-		meta:set_string("infotext", "Workbench")
-		meta:set_string("formspec", "size[9,9]"..
-						"list[current_player;craft;1.75,0.5;3,3;]"..
-						"list[current_player;craftpreview;5.75,1.5;1,1;]"..
-						"list[current_player;main;0,8;9,1;]"..
-						"list[current_player;main;0,5;9,3;9]"..
-						"image[4.75,1.5;1,1;sfinv_crafting_arrow.png]"..
-						"listring[current_player;main]"..
-						"listring[current_player;craft]")
-	end,
-})
-
-engine.register_craft({
-	output = "default:workbench",
-	recipe = {
-		{"default:wood", "default:wood"},
-		{"default:wood", "default:wood"},
-	}
-})
+	engine.register_node(_workbench.name, _workbench)
+	engine.register_craft({output = _workbench.name,
+												 recipe = {{X, X},
+																	 {X, X}}
+												})
+end
